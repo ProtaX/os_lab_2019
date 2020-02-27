01b2b241e4150a08aa19c3babc54153ecceaf051
Arseniy Karpenko <karpenko1298@gmail.com>
2020-02-27 22:32:28 +0300

    tabs change to spaces

--- lab3/src/main.c
+++ lab3/src/main.c
@@ -7,11 +7,11 @@
 #define SEQUENTIAL_BIN_NAME "sequential_min_max.out"
 
 int main(int argc, char* argv[]) {
-	if (access(SEQUENTIAL_BIN_NAME, F_OK) == -1) {
-		printf("Error: " SEQUENTIAL_BIN_NAME " not found\n");
-		return -1;
-	}
+  if (access(SEQUENTIAL_BIN_NAME, F_OK) == -1) {
+    printf("Error: " SEQUENTIAL_BIN_NAME " not found\n");
+    return -1;
+  }
 
-	execvp("./" SEQUENTIAL_BIN_NAME, argv);
-	printf("Error: execvp() failed, errno=%d\n", errno);
+  execvp("./" SEQUENTIAL_BIN_NAME, argv);
+  printf("Error: execvp() failed, errno=%d\n", errno);
 }
